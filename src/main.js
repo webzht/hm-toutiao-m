319c69599181d1c396d82b7e225fa7eda9f7f9c6
tengtengcai <2373099718@qq.com>
2020-03-15 20:29:40 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css' // 引入vant的样式
+Vue.use(Vant) // 注册Vant的所有组件
 Vue.config.productionTip = false
 
 new Vue({
